44f4717ae5e74f707f374594ce238ff451ed7bea
Jairo <55369888+ako2cjairo1@users.noreply.github.com>
2023-05-01 01:14:54 +0800

    Update constant.ts
    set cookieOptions (lax)

--- password-manager/server/src/constant.ts
+++ password-manager/server/src/constant.ts
@@ -43,12 +43,11 @@ export const TokenExpiration = {
 
 export const DefaultCookieOptions: CookieOptions = {
 	httpOnly: true, // set to "true" if we don't want want JS to read cookies
-	secure: isProd,
-	sameSite: "none",
+	secure: true,
+	sameSite: "lax",
 	maxAge: 15 * 60 * 1000,
 	path: "/", // to let cookies be available to all pages of our app
 	domain: COOKIE_DOMAIN,
-	// signed: true,
 }
 
 export const RateLimitConfig = {
