b4e6982d4024081d9b33baa5419781c913411e8d
ako2cjairo1 <ako2cjairo@gmail.com>
2023-04-30 23:42:58 +0800

    fix: update domain and secure CookieOptions

--- password-manager/server/src/constant.ts
+++ password-manager/server/src/constant.ts
@@ -43,11 +43,11 @@ export const TokenExpiration = {
 
 export const DefaultCookieOptions: CookieOptions = {
 	httpOnly: true, // set to "true" if we don't want want JS to read cookies
-	secure: isProd,
+	secure: true,
 	maxAge: 15 * 60 * 1000,
 	path: "/", // to let cookies be available to all pages of our app
+	domain: COOKIE_DOMAIN,
 	// sameSite: isProd ? "strict" : "lax",
-	// domain: COOKIE_DOMAIN,
 	// signed: true,
 }
 
