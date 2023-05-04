fe25a2414f4019d997e4e393d7239f09c754c55e
ako2cjairo1 <ako2cjairo@gmail.com>
2023-05-04 16:50:24 +0800

    WIP: set axios timeout, minor UI adjustment on KeychainCard

--- password-manager/client/src/api/axios.ts
+++ password-manager/client/src/api/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from 'axios'
 // extract api info from environment vars
 const { VITE_PUBLIC_API_BASEURL, VITE_PUBLIC_API_ENDPOINT } = import.meta.env
 // format API endpoint
-const baseURL = `${VITE_PUBLIC_API_BASEURL}${VITE_PUBLIC_API_ENDPOINT}`
+export const baseURL = `${VITE_PUBLIC_API_BASEURL}${VITE_PUBLIC_API_ENDPOINT}`
 
 export const requestConfig: AxiosRequestConfig = {
 	// to make sure the cookie is set
@@ -12,4 +12,5 @@ export const requestConfig: AxiosRequestConfig = {
 
 export default axios.create({
 	baseURL,
+	timeout: 5000,
 })
